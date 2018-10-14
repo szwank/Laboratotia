43f8e61402219205e428a90a79b088c31a52536b
Szwank-PC\Szwank <c@ijk.pl>
2018-10-14 12:41:23 +0200

    Poprawki w sciaga

--- Struktury i Algorytmy Sterowania/Lab1- Rekursywny filtr kalmana/moc_szumu.m
+++ Struktury i Algorytmy Sterowania/Lab1- Rekursywny filtr kalmana/moc_szumu.m
@@ -1,13 +1,15 @@
-% signal = zeros(10000000,1);    
-% noiseSignal =  awgn(signal, 10 * log10(0.1));
+% noiseSignal =  wgn(100000,1, 10 * log10(0.1));
 moc = 0;
+srednia = 0;
 for i = 1:length(simout.Data) - 1
     
-    moc = 0.001 * simout.Data(i)^2 + moc;
+    moc = 0.0001 * simout.Data(i)^2 + moc;
+    srednia = simout.Data(i) + srednia;
     
     
 end
 
-moc/1000
+moc / length(simout.Data)
+srednia / length(simout.Data)
 
 

3e7aa5ed8c14af061c5b5ea9ced14f3b94695db8
Szwank-PC\Szwank <c@ijk.pl>
2018-10-23 22:20:04 +0200

    Zadanie nr 3 zrealizowane oprócz przedostatniego podpunktu

--- Monitorowanie i diagnostyka w systemach sterowania/Lab2-3 - Analiza Składników Głównych (PCA)/zad3_Model_PCA.m
+++ Monitorowanie i diagnostyka w systemach sterowania/Lab2-3 - Analiza Składników Głównych (PCA)/zad3_Model_PCA.m
@@ -25,7 +25,7 @@ ylim([-5, 12])
 grid on
 
 [macierz_wektorow_wlasnych, D] = eig(R);
-wektor_wartosci_wlasnych = eig(R);
+wektor_wartosci_wlasnych = eig(R)';
 
 
 %% Przekszta³cenie do nowych wspó³¿êdnych
@@ -53,18 +53,50 @@ xlim([-2, 12])
 ylim([-5, 12])
 title('X PCAL');
 grid on
-%% Wyznaczenie SPE
+
+%% Wyznaczenie SPE dla X PCA
 SPE = [];
 for i = 1:length(X(:,1))
     
-   a = (X_norm(i,:) * (macierz_wektorow_wlasnych - [zredukowana_macierz_wektorow_wlasnych, zeros(2,1)]));
+   a = X_norm(i,:) * (macierz_wektorow_wlasnych - macierz_wektorow_wlasnych);
    SPE(i) = (a * a') ^ 2;
 end
 
+%% Wyznaczenie T2 dla X PCA
+T2 = X_PCA * wektor_wartosci_wlasnych' * X_PCAL';
+
+%% Wyznaczenie SPE dla X PCAL
+SPE_L = [];
+for i = 1:length(X(:,1))
+    
+   a = X_norm(i,:) * (macierz_wektorow_wlasnych - [zredukowana_macierz_wektorow_wlasnych, zeros(2,1)]);
+   SPE_L(i) = (a * a') ^ 2;
+end
+
 %% Wyznaczenie T2 dla X PCAL
+T2_L = X_PCAL * zredukowany_wektor_wartosci_wlasnych * X_PCAL';
 
-T2 = X_PCAL * zredukowany_wektor_wartosci_wlasnych * X_PCAL';
+%% wykresiki SPE I T2(jak to zrobiæ dla T2?????) <-- ogarn¹æ
+figure(2)
+subplot(2,2,1)
+plot(SPE ,zeros(1, length(SPE)), 'r*')
+title('SPE')
+subplot(2,2,2)
+plot(SPE_L ,zeros(1, length(SPE_L)), 'b*')
+title('SPE dla zredukowanego PCA')
 
+%% Rekonstrukcja danych
+X_zrekonstruowany = X_PCA * macierz_wektorow_wlasnych';
+XL_zrekunstruowany = X_PCAL * zredukowana_macierz_wektorow_wlasnych';
+
+% wykresik X_zrekonstruowany i XL_zrekunstruowany
+figure(3)
+plot(X_zrekonstruowany(:,1), X_zrekonstruowany(:,2), 'r*',XL_zrekunstruowany(:,1),XL_zrekunstruowany(:,2),'b*')
+hold on
+plot(X_norm(1),X_norm(2), 'g*')
+title('Zrekonstruowane dane')
+legend('Zrekonstruowane dane', 'Zrekonstruowane dane dla zredukowanego PCA', 'Znormalizowane dane')
+grid on
 
 
 

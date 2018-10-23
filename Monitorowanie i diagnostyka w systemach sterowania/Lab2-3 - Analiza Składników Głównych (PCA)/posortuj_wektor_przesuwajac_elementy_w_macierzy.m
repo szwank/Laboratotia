63e89ee22b212c4b29cad2462024e734d1cb8d12
Szwank-PC\Szwank <c@ijk.pl>
2018-10-23 21:47:55 +0200

    Poprawienie buga przy funkcij sortowania macieierzy, oraz zamianie elementów w macierzy

--- Monitorowanie i diagnostyka w systemach sterowania/Lab2-3 - Analiza Składników Głównych (PCA)/posortuj_wektor_przesuwajac_elementy_w_macierzy.m
+++ Monitorowanie i diagnostyka w systemach sterowania/Lab2-3 - Analiza Składników Głównych (PCA)/posortuj_wektor_przesuwajac_elementy_w_macierzy.m
@@ -9,22 +9,21 @@ end
 wektor_wartosci_wlasnych_kopja = wektor_wartosci_wlasnych;  % przekopjowanie wektora, dzia³anie na kopij
 posortowana_macierz_wekorow_wlasnych = macierz_wekorow_wlasnych;          
 
-posortowany_wektor_wartosci_wlasnych = [];                  % zainicjowanie
+posortowany_wektor_wartosci_wlasnych = wektor_wartosci_wlasnych;                  % zainicjowanie
 
-for j = 1:length(wektor_wartosci_wlasnych_kopja)
-        max = wektor_wartosci_wlasnych_kopja(1);           % przypisanie pierwszego elementu jako najwiêkszego
-        miejsce = 1;                                % przypisanie 1 miejsca do usuniêcia
-    for i = 1:length(wektor_wartosci_wlasnych_kopja)      % pêtla znajduje najwiekszy pozosta³y element
+for j = 1:length(wektor_wartosci_wlasnych_kopja)-1
+        max = posortowany_wektor_wartosci_wlasnych(j);           % przypisanie pierwszego elementu jako najwiêkszego
+        miejsce = j;                                % przypisanie 1 miejsca do usuniêcia
+    for i = j:length(posortowany_wektor_wartosci_wlasnych)      % pêtla znajduje najwiekszy pozosta³y element
                 
-        if max < wektor_wartosci_wlasnych_kopja(i)        % sprawdzenie czy nastêpny element jest wiêkszy
-            max = wektor_wartosci_wlasnych_kopja(i);      % aktualizacja zmiennych
+        if max < posortowany_wektor_wartosci_wlasnych(i)        % sprawdzenie czy nastêpny element jest wiêkszy
+            max = posortowany_wektor_wartosci_wlasnych(i);      % aktualizacja zmiennych
             miejsce = i;
         end
     end
     
-    posortowany_wektor_wartosci_wlasnych(j) = max;  % wpisanie znalezionej wartoœci do wektora
-    wektor_wartosci_wlasnych_kopja(miejsce) = [];               % usuniêcie wartoœci z wektora
-    posortowana_macierz_wekorow_wlasnych = zamien_kolumny_wierszami(posortowana_macierz_wekorow_wlasnych, miejsce, j);
+    posortowany_wektor_wartosci_wlasnych = zamien_kolumny_miejscami(posortowany_wektor_wartosci_wlasnych, miejsce, j);  % wpisanie znalezionej wartoœci do wektora
+    posortowana_macierz_wekorow_wlasnych = zamien_kolumny_miejscami(posortowana_macierz_wekorow_wlasnych, miejsce, j);
 end
 
 end
--- Monitorowanie i diagnostyka w systemach sterowania/Lab2-3 - Analiza Składników Głównych (PCA)/zamien_kolumny_miejscami.m
+++ Monitorowanie i diagnostyka w systemach sterowania/Lab2-3 - Analiza Składników Głównych (PCA)/zamien_kolumny_miejscami.m
@@ -1,7 +1,8 @@
-function [ zamieniona_macierz ] = zamien_kolumny_wierszami( macierz, numer_pierwszej_kolumny, numer_drugiej_kolumny )
-%Funkcja zamienia dwie kolumny macierzy miejscami
+function [ zamieniona_macierz ] = zamien_kolumny_miejscami( macierz, numer_pierwszej_kolumny, numer_drugiej_kolumny )
+%Funkcja zamienia dwie kolumny macierzy miejscami. je¿eli jako macierz
+%podany jest wektor, musi puæ poziomy.
 
-if numer_pierwszej_kolumny > length(macierz(1,:))
+if numer_pierwszej_kolumny > length(macierz)
     error('Numer pierwszej kolumny wychodzi poza rozmier macierzy')
     return;
 elseif numer_drugiej_kolumny > length(macierz(1,:))

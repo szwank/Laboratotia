09b6cbac1e4e5a3bfefa83337c490217f3d6bd7b
Szwank-PC\Szwank <c@ijk.pl>
2018-10-24 16:47:45 +0200

    Dodanie wykresów dla danych testowych, praca nad nałożeniem ich na dane trenujące. poprawienie obliczania SPE i T2

--- Monitorowanie i diagnostyka w systemach sterowania/Lab2-3 - Analiza Składników Głównych (PCA)/zad3_Model_PCA.m
+++ Monitorowanie i diagnostyka w systemach sterowania/Lab2-3 - Analiza Składników Głównych (PCA)/zad3_Model_PCA.m
@@ -63,7 +63,10 @@ for i = 1:length(X(:,1))
 end
 
 %% Wyznaczenie T2 dla X PCA
-T2 = X_PCA * wektor_wartosci_wlasnych' * X_PCAL';
+
+
+T2 = X_PCA * inv(diag(wektor_wartosci_wlasnych)) * X_PCA';
+T2 = diag(T2);
 
 %% Wyznaczenie SPE dla X PCAL
 SPE_L = [];
@@ -74,16 +77,32 @@ for i = 1:length(X(:,1))
 end
 
 %% Wyznaczenie T2 dla X PCAL
-T2_L = X_PCAL * zredukowany_wektor_wartosci_wlasnych * X_PCAL';
 
-%% wykresiki SPE I T2(jak to zrobiæ dla T2?????) <-- ogarn¹æ
+T2_L = X_PCAL * inv(diag(zredukowany_wektor_wartosci_wlasnych)) * X_PCAL';
+T2_L = diag(T2_L);
+
+%% wykresiki SPE I T2
 figure(2)
+
 subplot(2,2,1)
-plot(SPE ,zeros(1, length(SPE)), 'r*')
+plot(1:length(SPE), SPE, 'r*')
 title('SPE')
+grid on
+
 subplot(2,2,2)
-plot(SPE_L ,zeros(1, length(SPE_L)), 'b*')
+plot(1:length(SPE_L), SPE_L,'b*')
 title('SPE dla zredukowanego PCA')
+grid on
+
+subplot(2,2,3)
+plot(1:length(T2), T2, 'b*')
+title('T2')
+grid on
+
+subplot(2,2,4)
+plot(1:length(T2_L), T2_L ,'b*')
+title('T2 dla zredukowanego PCA')
+grid on
 
 %% Rekonstrukcja danych
 X_zrekonstruowany = X_PCA * macierz_wektorow_wlasnych';
@@ -98,7 +117,55 @@ title('Zrekonstruowane dane')
 legend('Zrekonstruowane dane', 'Zrekonstruowane dane dla zredukowanego PCA', 'Znormalizowane dane')
 grid on
 
+%% Miary SPE i T2 dla danych testowych
 
+% SPE
+SPE_T = [];
+for i = 1:length(XT_norm(:,1))
+    
+   a = XT_norm(i,:) * (macierz_wektorow_wlasnych - macierz_wektorow_wlasnych);
+   SPE_T(i) = (a * a') ^ 2;
+end
 
+% SPE reduced
+SPE_TL = [];
+for i = 1:length(XT_norm(:,1))
+    
+   a = XT_norm(i,:) * (macierz_wektorow_wlasnych - [zredukowana_macierz_wektorow_wlasnych, zeros(2,1)]);
+   SPE_TL(i) = (a * a') ^ 2;
+end
 
+% T2
+T2_T = XT_norm * macierz_wektorow_wlasnych * inv(diag(wektor_wartosci_wlasnych)) * macierz_wektorow_wlasnych' * XT_norm';
+T2_T = diag(T2_T);
 
+% T2 reduced
+T2_TL = XT_norm * zredukowana_macierz_wektorow_wlasnych * inv(diag(zredukowany_wektor_wartosci_wlasnych)) * zredukowana_macierz_wektorow_wlasnych' * XT_norm';
+T2_TL = diag(T2_TL);
+
+% Wykresik dla danych testowych
+
+figure(4)
+subplot(2,2,1)
+plot(1:length(SPE_T), SPE_T, 'r*', length(SPE_T) + 1 :length(SPE), SPE, 'b*')
+title('WskaŸnik SPE- dane testowe, R = 2')
+legend('Dane testowe', 'Dane treningowe')
+grid on
+
+subplot(2,2,2)
+plot(1:length(SPE_TL), SPE_TL, 'r*', length(SPE_TL) + 1 :length(SPE_L) , SPE_L, 'b*' )
+title('WskaŸnik SPE- dane testowe, R = 1')
+legend('Dane testowe', 'Dane treningowe')
+grid on
+
+subplot(2,2,3)
+plot(1: length(T2_T), T2_T, 'r*', length(T2_T) + 1 : length(T2), T2, 'b*')
+title('WskaŸnik T2- dane testowe, R = 2')
+legend('Dane testowe', 'Dane treningowe')
+grid on
+
+subplot(2,2,4)
+plot(1: length(T2_TL), T2_TL, 'r*', length(T2_TL) + 1 : length(T2_L), T2_L, 'b*')
+title('WskaŸnik T2- dane testowe, R = 1')
+legend('Dane testowe', 'Dane treningowe')
+grid on
